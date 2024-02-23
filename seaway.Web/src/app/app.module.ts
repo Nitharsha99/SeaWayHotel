e29716193235af89d609bc2afe00a803b7d0c04a
Nitharsha99 <nitharshathev@gmail.com>
2024-02-23 20:25:09 +0530

    complete 95% of home page design

--- seaway.Web/src/app/app.module.ts
+++ seaway.Web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
