853963db313c00511ab5c1153c5c5d4762c68848
Nitharsha99 <nitharshathev@gmail.com>
2024-04-29 23:43:17 +0530

    completed delete function of room

--- seaway.Web/src/app/Components/Admin/rooms/all-rooms/all-rooms.component.ts
+++ seaway.Web/src/app/Components/Admin/rooms/all-rooms/all-rooms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Room } from 'src/app/Models/room';
 import { RoomService } from 'src/app/Services/RoomService/room.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-all-rooms',
@@ -26,7 +27,39 @@ export class AllRoomsComponent implements OnInit{
     this.router.navigate(['addRoom'], {relativeTo: this.route});
   }
 
-  navigateToUpdate(){}
+  navigateToUpdate(id: number){
+    console.log("id", id);
+    this.router.navigate(['editRoom', id], {relativeTo: this.route});
+  }
 
-  deleteRoom(){}
+  deleteRoom(id: number){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'No, keep it',
+      confirmButtonText: 'Yes, delete it!',
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      iconColor: "#d33"
+  }).then((result) => {
+      if (result.isConfirmed) {
+       this.roomService.DeleteRoom(id).subscribe(res => {
+        Swal.fire({
+          icon: "success",
+          title: "Successfully Deleted " + res.roomName + " !!! ",
+          showConfirmButton: false,
+          timer: 1700
+        });
+        setTimeout(() => {
+          window.location.reload();
+        });
+       });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire('Process Cancelled', 'Your Record is safe now !!');
+      }
+  });
+  }
+
 }
