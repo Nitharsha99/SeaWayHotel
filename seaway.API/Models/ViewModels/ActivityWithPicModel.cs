5a79041d3755b3afcc7e193b5aa16c0e2e0c852f
Nitharsha99 <nitharshathev@gmail.com>
2024-05-25 01:19:48 +0530

    SEA-14 : Create SP for new activity insert function SEA-15 : Create Function for new activity in BE Completed

--- seaway.API/Models/ViewModels/ActivityWithPicModel.cs
+++ seaway.API/Models/ViewModels/ActivityWithPicModel.cs
@@ -5,6 +5,13 @@
         public string? ActivityName { get; set; }
         public string? Description { get; set; }
         public bool ActivityIsActive { get; set; }
-        public IFormFile[]? PicValue { get; set; }
+        public ActivityPic[]? ActivityPics { get; set; }
+    }
+
+    public class ActivityPic
+    {
+        public string? PicName { get; set; }
+        public string? PicValue { get; set; }
+        public string? CloudinaryPublicId { get; set; }
     }
 }
