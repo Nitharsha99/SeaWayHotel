3ae6852d885425a87a2178bd5fbef6481287dd8c
Nitharsha99 <nitharshathev@gmail.com>
2024-05-29 15:55:24 +0530

    Create Offer controllers and develop only the controller part : SEA- 29

---
+++ seaway.API/Controllers/OfferController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using seaway.API.Configurations;
+using seaway.API.Manager;
+using seaway.API.Models;
+
+namespace seaway.API.Controllers
+{
+    [Route("api/Offer")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        private readonly ILogger<LogHandler> _logger;
+        private readonly IConfiguration _configuration;
+        LogHandler _log;
+        OfferManager _offerManager;
+
+        public OfferController(ILogger<LogHandler> logger, IConfiguration configuration, LogHandler log, OfferManager offerManager)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _log = log;
+            _offerManager = offerManager;
+        }
+
+        [HttpPatch]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult ChangeActiveStatus(bool status, int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    bool isStatusChanged = false;
+                    Offer offer = _offerManager.GetOfferById(id);
+
+                    if (offer.OfferName != null)
+                    {
+                        isStatusChanged = _offerManager.ChangeActiveStatus(status, id);
+
+                        if (isStatusChanged)
+                        {
+                            return Ok(true);
+                        }
+                        else
+                        {
+                            return BadRequest("Error on changing offer status");
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("There is no any offer with this Id --> " + id);
+                    }
+                }
+                return BadRequest("Id is not Supplied");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("An exception occurred while changing Active status of Offer : " + ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ seaway.API/Models/Offer.cs
@@ -0,0 +1,16 @@
+﻿namespace seaway.API.Models
+{
+    public class Offer
+    {
+        public int OfferId { get; set; }
+        public string? OfferName { get; set; }
+        public string? Description { get; set;}
+        public double? Price { get; set; }
+        public double? DiscountPercentage { get; set; }
+        public double? DiscountAmount { get; set; }
+        public DateTime? ValidFrom { get; set; }
+        public DateTime? ValidTo { get; set;}
+        public bool? IsActive { get; set; }
+        public bool? IsRoomOffer { get; set;}
+    }
+}
