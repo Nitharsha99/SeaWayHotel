bbc6293d22aa3d37586d7a195d8ec4c512a230b6
Nitharsha99 <nitharshathev@gmail.com>
2024-07-05 18:33:00 +0530

    SEA-51: Develop Activity Form UI

--- seaway.Web/src/app/Components/Admin/activities/add-activities/add-activities.component.ts
+++ seaway.Web/src/app/Components/Admin/activities/add-activities/add-activities.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PicDocument } from 'src/app/Models/picDocument';
 
 @Component({
   selector: 'app-add-activities',
   templateUrl: './add-activities.component.html',
   styleUrls: ['./add-activities.component.css']
 })
-export class AddActivitiesComponent {
+export class AddActivitiesComponent implements OnInit{
 
+  updateMode: boolean = true;
+  pictures: PicDocument[] = [];
+  imageWidth: number = 170;
+  imageMargin: number = 2;
+  imageHeight: number = 110;
+  files: File[] = [];
+  selectedPictures: string[] = [];
+
+  constructor(private builder: FormBuilder){}
+
+  activityForm: FormGroup = this.builder.group({
+    activityName:[''],
+    description:['']
+  })
+
+  ngOnInit(): void {
+    
+  }
+
+  addActivity(){
+
+  }
+
+  updateSelectedPics(id: string): void{
+
+  }
+
+  deleteImages(): void{
+
+  }
+
+  onSelect(event: any): void{
+
+  }
+
+  onRemove(event: any): void{
+
+  }
+
+  redirectToBack(): void{
+
+  }
+
+  resetForm(): void{
+
+  }
 }
