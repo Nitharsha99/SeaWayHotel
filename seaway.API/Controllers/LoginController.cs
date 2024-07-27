6e8167b2b8a9739c9af474587fec70fe8a965c76
Nitharsha99 <nitharshathev@gmail.com>
2024-07-28 00:22:16 +0530

    SEA 97: Get the admin data from DB and compare the password SEA-91: Change Login Function in BE SEA-9: Change login based on admin pic

--- seaway.API/Controllers/LoginController.cs
+++ seaway.API/Controllers/LoginController.cs
@@ -2,86 +2,62 @@
 using seaway.API.Models;
 using seaway.API.Manager;
 using seaway.API.Configurations;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Authorization;
-using System.IdentityModel.Tokens.Jwt;
+using seaway.API.Models.ViewModels;
+using Newtonsoft.Json;
 
 namespace seaway.API.Controllers
 {
-    [Route("api/login")]
+    [Route("api/Login")]
     [ApiController]
     public class LoginController : ControllerBase
     {
-        //private readonly ILogger<LogHandler> _logger;
-        //private readonly IConfiguration _configuration;
-        //LoginManager _logginManager;
-        //LogHandler _log;
+        private readonly ILogger<LogHandler> _logger;
+        private readonly IConfiguration _configuration;
+        LoginManager _logginManager;
+        LogHandler _log;
 
-        //public LoginController(ILogger<LogHandler> logger, IConfiguration configuration)
-        //{
-        //    _logger = logger;
-        //    _configuration = configuration;
-        //    _logginManager = new LoginManager(logger, configuration);
-        //    _log = new LogHandler(logger);
-        //}
+        public LoginController(ILogger<LogHandler> logger, IConfiguration configuration, LoginManager loginManager)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _logginManager = loginManager;
+            _log = new LogHandler(logger);
+        }
 
-        //[Route("")]
-        //[HttpGet]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //public async Task<IActionResult> GetAdmins()
-        //{
-        //    try
-        //    {
-        //        List<Admin> adminList = _logginManager.GetAdmin();
+        [HttpPost]
+        [Route("Login")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Login(LoginModel login)
+        {
+            try
+            {
+                if (login.Username == null)
+                {
+                    return BadRequest(DisplayMessages.NullInput);
+                }
+                else
+                {
+                    bool isValidUser = false;
+     
+                    isValidUser = _logginManager.CheckUserValid(login);
 
-        //        string responseBody = JsonConvert.SerializeObject(adminList);
+                    string responseBody = JsonConvert.SerializeObject(login);
+                    string requestUrl = HttpContext.Request.Path.ToString();
 
-        //        string requestUrl = HttpContext.Request.Path.ToString();
+                     _log.setLogTrace(new HttpRequestMessage(), new HttpResponseMessage(), responseBody, requestUrl);
 
-        //        _log.setLogTrace(new HttpRequestMessage(), new HttpResponseMessage(), responseBody, requestUrl);
+                    return Ok(isValidUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(LogMessages.LoginError + ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
 
 
-        //        return Ok(adminList);
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        _logger.LogError("An exception occurred while retrieving admins : " + ex.Message);
-        //        return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
-        //    }
-        //}
-
-        //[HttpPost]
-        //[Route("Login")]
-        //[ProducesResponseType(StatusCodes.Status201Created)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //public IActionResult Login(Admin admin)
-        //{
-        //    try
-        //    {
-        //        if( admin == null)
-        //        {
-        //            return BadRequest("login credentials are empty");
-        //        }
-        //        else
-        //        {
-        //            bool isValidUser = false;
-        //            if(admin.Password != null)
-        //            {
-        //                isValidUser = _logginManager.CheckUserValid(admin);
-        //            }
-        //            return Ok(isValidUser);
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError("An exception occurred while login : " + ex.Message);
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-    
     }
 }
 
