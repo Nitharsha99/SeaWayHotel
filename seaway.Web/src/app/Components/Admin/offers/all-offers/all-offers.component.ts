d22a3654188aaa4420897e0134dac7c5dda1e4d1
Nitharsha99 <nitharshathev@gmail.com>
2024-08-08 20:39:37 +0530

    SEA-55: Mapping Data to Offer List in FE

--- seaway.Web/src/app/Components/Admin/offers/all-offers/all-offers.component.ts
+++ seaway.Web/src/app/Components/Admin/offers/all-offers/all-offers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommonFunctionComponent } from 'src/app/commonFunction';
+import { Offer } from 'src/app/Models/offer';
+import { OfferService } from 'src/app/Services/OfferService/offer.service';
 
 @Component({
   selector: 'app-all-offers',
@@ -9,15 +12,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AllOffersComponent implements OnInit{
   pageSize: number = 5;
   page: number = 1;
+  offers: Offer[] = [];
+  filteredOffers: Offer[] = [];
+  search = '';
 
-  constructor(private router: Router, private route: ActivatedRoute){}
+  constructor(private router: Router, private route: ActivatedRoute,
+              private offerService: OfferService, private commonFunction: CommonFunctionComponent
+  ){}
 
   ngOnInit(): void {
-    
+    this.offerService.GetOffers().subscribe(res => {
+      this.offers = res;
+      this.filteredOffers = this.offers;
+    })
   }
 
   onSearch(): void{
-
+    this.filteredOffers = this.offers.filter(o => {
+      return o.name.toLowerCase().includes(this.search.toLowerCase());
+    });
   }
 
   navigateToAddOffer(): void{
