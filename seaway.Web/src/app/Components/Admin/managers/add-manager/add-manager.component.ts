b94335b55d17d65fa808f267f359d2fd9838d70a
Nitharsha99 <nitharshathev@gmail.com>
2024-10-09 20:02:26 +0530

    SEA-162: Manager Form UI

--- seaway.Web/src/app/Components/Admin/managers/add-manager/add-manager.component.ts
+++ seaway.Web/src/app/Components/Admin/managers/add-manager/add-manager.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-manager',
   templateUrl: './add-manager.component.html',
   styleUrls: ['./add-manager.component.css']
 })
-export class AddManagerComponent {
+export class AddManagerComponent implements OnInit{
 
+  updateMode: boolean = false;
+  profilePic: string = "../../../../../assets/images/profile.jpg";
+
+  constructor(private builder: FormBuilder, private router: Router,  private route: ActivatedRoute){}
+
+  managerForm: FormGroup = this.builder.group({
+    username:[''],
+    password:[''],
+    createdBy: ['Nitharsha'],
+    updatedBy: [''],
+    picName: [null],
+    picValue: [null],
+    cloudinaryPublicId: [null]
+  });
+
+  ngOnInit(): void {
+    
+  }
+
+  onSelectProfilePic(event: any): void{
+
+  }
+
+  redirectToBack(): void{
+
+  }
+
+  resetForm(): void{
+
+  }
+
+  addManager(): void{
+
+  }
 }
